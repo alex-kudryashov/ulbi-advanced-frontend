00adbff1bca25599c8bea5ce67325728f0cbc8c5
Alexandr <kudryashov_aa@resanta.ru>
2024-04-22 16:00:47 +0300

    3.2

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import webpack, { WebpackPluginInstance } from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import ReactRefreshPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 export default (options: BuildOptions):WebpackPluginInstance[] => {
@@ -18,5 +19,8 @@ export default (options: BuildOptions):WebpackPluginInstance[] => {
             __IS_DEV__: JSON.stringify(isDev),
         }),
         isDev && new ReactRefreshPlugin(),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }),
     ].filter(Boolean);
 };
